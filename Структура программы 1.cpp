40878c4f9f4971e5aca3b06c7fa667f5da424cac
BajdyukovaOlesya <92226132+BajdyukovaOlesya@users.noreply.github.com>
2021-11-03 15:16:33 +0500

    Add files via upload

---
+++ Структура программы 1.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+using namespace std;
+int i=1,a; // Объявление переменных + присваивание одной их них значения
+int main()
+{
+    a = ++i * i++; 
+    cout << a; // Вывод значения выражения
+    return 0;
+}
